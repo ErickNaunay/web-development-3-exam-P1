d69b2ae004194ba26ae4e4f57345f264ddee28c6
ErickNaunay <enaunay@gmail.com>
2020-11-04 16:02:27 -0500

    feature: add cors options

--- app.js
+++ app.js
@@ -1,43 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
 
-require('./app_api/models/db')
+require("./app_api/models/db");
 
-var indexRouter = require('./app_server/routes/index');
-var apiRouter = require('./app_api/routes/index');
+var indexRouter = require("./app_server/routes/index");
+var apiRouter = require("./app_api/routes/index");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, './app_server/views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "./app_server/views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/api/v1', apiRouter);
+// eventually there will be more than one domain allowed
+const allowedList = ["http://localhost:3000"];
+const corsOptionsDelegate = function handler(req, callback) {
+  const corsOptions = { origin: false };
+
+  if (allowedList.indexOf(req.header("Origin") ?? "") !== -1) {
+    corsOptions.origin = true;
+  }
+  callback(null, corsOptions);
+};
+
+app.use(cors(corsOptionsDelegate));
+
+app.use("/", indexRouter);
+app.use("/api/v1", apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
